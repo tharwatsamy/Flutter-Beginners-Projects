89f42c03ac681139f8c9727bf67e58f484a7d566
tharwatsamy <tharwatsamy2004@gmail.com>
2022-08-15 10:04:36 +0200

    update weather app

--- weather_app_setup-main/lib/models/weather_model.dart
+++ weather_app_setup-main/lib/models/weather_model.dart
@@ -1,71 +1,73 @@
-import 'dart:developer';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WeatherModel {
-  String weatherStateName;  
   DateTime date;
   double temp;
-  double minTemp;
   double maxTemp;
+  double minTemp;
+  String weatherStateName;
 
-  WeatherModel({
-    required this.weatherStateName,
-    required this.date,
-    required this.temp,
-    required this.minTemp,
-    required this.maxTemp,
-  });
+  WeatherModel(
+      {required this.date,
+      required this.temp,
+      required this.maxTemp,
+      required this.minTemp,
+      required this.weatherStateName});
 
-  factory WeatherModel.fromJson(Map<String, dynamic> data) {
+  factory WeatherModel.fromJson(dynamic data) {
+    var jsonData = data['forecast']['forecastday'][0]['day'];
+
     return WeatherModel(
-      weatherStateName: data['weather_state_name'],
-      date:DateTime.parse( data['created']),
-      temp: data['the_temp'],
-      minTemp: data['min_temp'],
-      maxTemp: data['max_temp'],
-    );
+        date: DateTime.parse( data['current']['last_updated']),
+        temp: jsonData['avgtemp_c'],
+        maxTemp: jsonData['maxtemp_c'],
+        minTemp: jsonData['mintemp_c'],
+        weatherStateName: jsonData['condition']['text']);
   }
 
-  String getImage() {
-    if (weatherStateName == 'Clear' || weatherStateName == 'Light Cloud') {
+  @override
+  String toString() {
+    return 'tem = $temp  minTemp = $minTemp  date = $date';
+  }
+
+   String getImage() {
+    if (weatherStateName == 'Sunny' || weatherStateName == 'Clear' ||  weatherStateName == 'partly cloudy') {
       return 'assets/images/clear.png';
-    } else if (weatherStateName == 'Sleet' ||
-        weatherStateName == 'Snow' ||
-        weatherStateName == 'Hail') {
+    } else if (
+        
+        weatherStateName == 'Blizzard' ||  weatherStateName == 'Patchy snow possible'  ||  weatherStateName == 'Patchy sleet possible' || weatherStateName == 'Patchy freezing drizzle possible' || weatherStateName == 'Blowing snow') {
       return 'assets/images/snow.png';
-    } else if (weatherStateName == 'Heavy Cloud') {
+    } else if (weatherStateName == 'Freezing fog' || weatherStateName == 'Fog' ||  weatherStateName == 'Heavy Cloud' || weatherStateName == 'Mist' || weatherStateName == 'Fog') {
       return 'assets/images/cloudy.png';
-    } else if (weatherStateName == 'Light Rain' ||
+    } else if (weatherStateName == 'Patchy rain possible' ||
         weatherStateName == 'Heavy Rain' ||
         weatherStateName == 'Showers	') {
       return 'assets/images/rainy.png';
-    } else if (weatherStateName == 'Thunderstorm' || weatherStateName == 'Thunder') {
+    } else if (weatherStateName == 'Thundery outbreaks possible' || weatherStateName == 'Moderate or heavy snow with thunder' || weatherStateName == 'Patchy light snow with thunder'|| weatherStateName == 'Moderate or heavy rain with thunder' || weatherStateName == 'Patchy light rain with thunder' ) {
       return 'assets/images/thunderstorm.png';
     } else {
       return 'assets/images/clear.png';
     }
   }
-
- 
-  MaterialColor getThemeColor() {
-    if (weatherStateName == 'Clear' || weatherStateName == 'Light Cloud') {
+MaterialColor getThemeColor() {
+    if (weatherStateName == 'Sunny' || weatherStateName == 'Clear' ||  weatherStateName == 'partly cloudy') {
       return Colors.orange;
-    } else if (weatherStateName == 'Sleet' ||
-        weatherStateName == 'Snow' ||
-        weatherStateName == 'Hail') {
+    } else if (
+        
+        weatherStateName == 'Blizzard' ||  weatherStateName == 'Patchy snow possible'  ||  weatherStateName == 'Patchy sleet possible' || weatherStateName == 'Patchy freezing drizzle possible' || weatherStateName == 'Blowing snow') {
       return Colors.blue;
-    } else if (weatherStateName == 'Heavy Cloud') {
+    } else if (weatherStateName == 'Freezing fog' || weatherStateName == 'Fog' ||  weatherStateName == 'Heavy Cloud' || weatherStateName == 'Mist' || weatherStateName == 'Fog') {
       return Colors.blueGrey;
-    } else if (weatherStateName == 'Light Rain' ||
+    } else if (weatherStateName == 'Patchy rain possible' ||
         weatherStateName == 'Heavy Rain' ||
         weatherStateName == 'Showers	') {
       return Colors.blue;
-    } else if (weatherStateName == 'Thunderstorm' || weatherStateName == 'Thunder' ){
+    } else if (weatherStateName == 'Thundery outbreaks possible' || weatherStateName == 'Moderate or heavy snow with thunder' || weatherStateName == 'Patchy light snow with thunder'|| weatherStateName == 'Moderate or heavy rain with thunder' || weatherStateName == 'Patchy light rain with thunder' ) {
       return Colors.deepPurple;
     } else {
       return Colors.orange;
     }
   }
+
+  
 }
--- weather_app_setup-main/lib/services/weather_service.dart
+++ weather_app_setup-main/lib/services/weather_service.dart
@@ -4,37 +4,22 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
-  String baseUrl = 'https://www.metaweather.com';
+  String baseUrl = 'http://api.weatherapi.com/v1';
 
-  Future<int> getCityId({required String cityName}) async {
-    Uri url = Uri.parse('$baseUrl/api/location/search/?query=$cityName');
+  String apiKey = '3677bed892474b30b7a122242220806';
+  Future<WeatherModel> getWeather({required String cityName}) async {
+    Uri url =
+        Uri.parse('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=7');
     http.Response response = await http.get(url);
-    print(response.body);
-    List<dynamic> data = jsonDecode(response.body);
 
-    int cityId = data[0]['woeid'];
-
-    return cityId;
-  }
-
-  Future<WeatherModel?> getWeather({required String cityName}) async {
-   
-    WeatherModel? weatherData ;
-    try {
-      int id = await getCityId(cityName: cityName);
-
-      Uri url = Uri.parse('$baseUrl/api/location/$id/');
-      http.Response response = await http.get(url);
-
-      Map<String, dynamic> jsonData = jsonDecode(response.body);
-
-      Map<String, dynamic> data = jsonData['consolidated_weather'][0];
-
-    weatherData  = WeatherModel.fromJson(data);
-    } catch (e) {
-      print(e);
+    if (response.statusCode == 400) {
+    var data  = jsonDecode(response.body);
+      throw Exception(data['error']['message']);
     }
+    Map<String, dynamic> data = jsonDecode(response.body);
 
-    return weatherData;
+    WeatherModel weather = WeatherModel.fromJson(data);
+
+    return weather;
   }
 }
